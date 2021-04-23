5de6d5ea52bbe0aef745c019051bb87b04d2e2db
xiaoyueyuea <1406817218@qq.com>
2021-04-23 15:24:47 +0800

    @Transactional

---
+++ src/main/java/com/lay/designMode/Singleton.java
@@ -0,0 +1,17 @@
+package com.lay.designMode;
+
+/**
+ * @Version 1.0
+ * @Author lei.yue
+ * @Created 2021/4/8 11:37
+ * @Description <p>
+ * @Modification <p>
+ * Date Author Version Description
+ * <p>
+ * 2021/4/8 lei.yue 1.0 create file
+ */
+//单例模式
+public class Singleton {
+
+
+}
--- src/main/java/com/lay/threads/concurrentUtilKit/locks/Main.java
+++ src/main/java/com/lay/threads/concurrentUtilKit/locks/Main.java
@@ -1,6 +1,10 @@
 package com.lay.threads.concurrentUtilKit.locks;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -28,5 +32,28 @@ public class Main {
 //        exc.shutdown();
 //        exc.awaitTermination(1, TimeUnit.MINUTES);
 //        System.out.println(VolatileTest.data);
+
+
+        int max = 100;
+        Random random = new Random();
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+        List<Integer> c = new ArrayList<>();
+        for(int i = 0;i<100;i++){
+            int r = random.nextInt(max) + 1;
+            if(r > 0 && r <=10){
+                a.add(r);
+            }else if(r > 10 & r<=90){
+                b.add(r);
+            }else {
+                c.add(r);
+            }
+        }
+        System.out.println("A组：[" + a.size() + "]");
+        a.stream().sorted((e1,e2) -> e1.compareTo(e2)).forEach(e -> System.out.print(e + ","));
+        System.out.println("B组：[" + b.size() + "]");
+        b.stream().sorted((e1,e2) -> e1.compareTo(e2)).forEach(e -> System.out.print(e + ","));
+        System.out.println("C组：[" + c.size() + "]");
+        c.stream().sorted((e1,e2) -> e1.compareTo(e2)).forEach(e -> System.out.print(e + ","));
     }
 }
