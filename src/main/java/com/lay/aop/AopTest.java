95605d9ba6f1d41e07a8424eac6adba0558dd339
xiaoyueyuea <1406817218@qq.com>
2021-07-02 16:19:09 +0800

    aop

--- src/main/java/com/lay/aop/AopTest.java
+++ src/main/java/com/lay/aop/AopTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
 
 /**
  * @Version 1.0
--- src/main/java/com/lay/aop/LogAspects.java
+++ src/main/java/com/lay/aop/LogAspects.java
@@ -1,8 +1,8 @@
 package com.lay.aop;
 
-//import org.aspectj.lang.ProceedingJoinPoint;
-//import org.aspectj.lang.annotation.*;
-//import org.springframework.stereotype.Component;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
 
 /**
  * @Version 1.0
@@ -14,29 +14,34 @@ package com.lay.aop;
  * <p>
  * 2021/7/2 lei.yue 1.0 create file
  */
-//@Aspect
-//@Component
-//public class LogAspects {
-//
-//    @Pointcut("execution(public void com.lay.aop.JoinPointMethod.*(..))")
-//    public void pointCut(){}
-//
-//    //@before代表在目标方法执行前切入, 并指定在哪个方法前切入
-//    @Before("pointCut()")
-//    public void logBefore(){
-//        System.out.println("@Before");
-//    }
-//
-//    @After("pointCut()")
-//    public void logAfter(){
-//        System.out.println("@After");
-//    }
-//
-//    @Around("pointCut()")
-//    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-//        System.out.println("@Around:执行目标方法之前");
-//        Object obj = joinPoint.proceed();//手动执行目标方法
-//        System.out.println("@Around:执行目标方法之后");
-//        return obj;
-//    }
-//}
+@Aspect
+@Component
+public class LogAspects {
+
+    @Pointcut("execution(public void com.lay.aop.JoinPointMethod.*(..))")
+    public void pointCut(){}
+
+    //@before代表在目标方法执行前切入, 并指定在哪个方法前切入
+    @Before("pointCut()")
+    public void logBefore(){
+        System.out.println("@Before");
+    }
+
+    @After("pointCut()")
+    public void logAfter(){
+        System.out.println("@After");
+    }
+
+    @Around("pointCut()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("@Around:执行目标方法之前");
+        Object obj = joinPoint.proceed();//手动执行目标方法
+        System.out.println("@Around:执行目标方法之后");
+        return obj;
+    }
+
+    @AfterReturning("pointCut()")
+    public void logAfterReturning(){
+        System.out.println("@AfterReturning");
+    }
+}
