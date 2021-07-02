6297c77170155860c44b8577f394bcae36c5c9c6
xiaoyueyuea <1406817218@qq.com>
2021-07-02 10:49:06 +0800

    ceshi

--- src/main/java/com/lay/threads/concurrentUtilKit/atomic/Main.java
+++ src/main/java/com/lay/threads/concurrentUtilKit/atomic/Main.java
@@ -1,7 +1,10 @@
 package com.lay.threads.concurrentUtilKit.atomic;
 
+import java.math.BigDecimal;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -16,20 +19,45 @@ import java.util.concurrent.TimeUnit;
  */
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        // 创建线程池
+//        // 创建线程池
         ExecutorService exec = Executors.newCachedThreadPool();
-        //AtomicBoolean测试
-        exec.execute(new AtomicBooleanTest("Lay"));
-        exec.execute(new AtomicBooleanTest("Krispy"));
+//        //AtomicBoolean测试
+//        exec.execute(new AtomicBooleanTest("Lay"));
+//        exec.execute(new AtomicBooleanTest("Krispy"));
+//
+//        //AtomicReference测试
+//        for(int i = 0;i<1000000;i++){
+//            exec.execute(new AtomicReferenceTest());
+//        }
+//
+//        //等待线程池中所有线程执行完毕
+//        exec.shutdown();
+//        exec.awaitTermination(1, TimeUnit.MINUTES);
+//        System.out.println(AtomicReferenceTest.count);
 
-        //AtomicReference测试
-        for(int i = 0;i<1000000;i++){
-            exec.execute(new AtomicReferenceTest());
-        }
+//        List<String> strs = new ArrayList<>();
+//        String test = "";
+//        System.out.println(strs.contains(test));
+//        System.out.println(strs.contains(null));
 
-        //等待线程池中所有线程执行完毕
-        exec.shutdown();
-        exec.awaitTermination(1, TimeUnit.MINUTES);
-        System.out.println(AtomicReferenceTest.count);
+//        String[] strs = new String[]{"Lay","Krispy"};
+//        List<String> stringList = Arrays.asList(strs);
+//        stringList.add("测试");
+
+        Map<String,String> map = new HashMap<>();
+        map.put("me","Lay");
+        map.put("she","Krispy");
+        //map.keySet().forEach(System.out::println);
+        //map.values().forEach(System.out::println);
+//        map.entrySet().forEach(e -> {
+//            System.out.println(e.getKey() + "###" + e.getValue());
+//        });
+
+//        map.keySet().add("测试");
+//        map.values().add("测试");
+
+
+        BigDecimal bigDecimal = new BigDecimal("");
+        System.out.println(bigDecimal);
     }
 }
