722bf07f2dfb1a7f173e8ae073cde901ede5fe19
xiaoyueyuea <1406817218@qq.com>
2021-08-02 11:19:46 +0800

    懒加载

--- src/main/java/com/lay/designMode/Singleton.java
+++ src/main/java/com/lay/designMode/Singleton.java
@@ -13,5 +13,14 @@ package com.lay.designMode;
 //单例模式
 public class Singleton {
 
+    //实例声明为静态的私有的
+    private static Singleton instance = new Singleton();
 
+    //构造函数设为私有
+    private Singleton(){}
+
+    //静态工厂方法，供外部访问
+    public static Singleton getInstance(){
+        return instance;
+    }
 }
