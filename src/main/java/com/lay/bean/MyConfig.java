7ae7f65ed6598edebc28b0643682b10f74a15f28
xiaoyueyuea <1406817218@qq.com>
2021-09-03 15:29:34 +0800

    嵌套配置类

--- src/main/java/com/lay/bean/MyConfig.java
+++ src/main/java/com/lay/bean/MyConfig.java
@@ -1,5 +1,7 @@
 package com.lay.bean;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,5 +23,18 @@ public class MyConfig {
         ResultBean resultBean = new ResultBean();
         System.out.println("ResultBean----加入容器：" + resultBean);
         return resultBean;
+    }
+
+    //嵌套配置类必须声明为static
+    @Configuration
+    @ConditionalOnBean(name = "myConfig")
+    public static class InnerConfig{
+
+        @Bean(name = "anotherResult")
+        @ConditionalOnMissingBean(name = "Result")
+        public ResultBean getAnotherResult(){
+            System.out.println("嵌套配置类未找到Result");//静态内部类优先执行，此时getResultBean()还未执行
+            return new ResultBean();
+        }
     }
 }
