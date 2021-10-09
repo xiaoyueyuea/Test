61f45f417a2d909dd8c315bc693e4858ff107a71
xiaoyueyuea <1406817218@qq.com>
2021-10-09 16:32:37 +0800

    gitTest

--- src/main/java/com/lay/mq/RedisMQTest.java
+++ src/main/java/com/lay/mq/RedisMQTest.java
@@ -41,18 +41,6 @@ public class RedisMQTest {
 //        String name = (String) redisTemplate.boundValueOps("name").get();
 //        System.out.println(name);
 
-        List<String> str1 = new ArrayList<>();
-        List<String> str2 = new ArrayList<>();
-        str1.add("2");
-//        str2.add("1");
-//        str1.addAll(str2);
-//        System.out.println(str1);
-//        StringBuilder sb = new StringBuilder();
-//        if(sb.toString().equals("")){
-//            System.out.println(1);
-//        }else {
-//            System.out.println(0);
-//        }
 
 //        String a = null;
 //        String b = "2";
@@ -77,36 +65,6 @@ public class RedisMQTest {
 //        final LocalDateTime beginLocalDateTime = LocalDateTime.of(endLocalDateTime.getYear(),endLocalDateTime.getMonthValue(),1,0,0);
 //        System.out.println(beginLocalDateTime);
 
-//        Random random = new Random();
-//        for (int i = 0;i< 100;i++){
-//            System.out.println(random.nextInt(2));
-//        }
-
-//
-//        final List<String> status = new ArrayList<>();
-//        status.add("M1");
-//        status.add("M7");
-//        status.add("M3");
-//        status.add("M4");
-//
-//        String maxOverdueStatus = "N";
-//        int maxLevel = 0;
-//        for(String s : status){
-//            if(!s.equals("N")){
-//                int level = Integer.parseInt(s.replace("M",""));
-//                if(level > maxLevel){
-//                    maxOverdueStatus = s;
-//                    maxLevel = level;
-//                }
-//            }
-//        }
-//        System.out.println(maxOverdueStatus + "###" + maxLevel);
-
-//        Calendar calendar = Calendar.getInstance();
-//        calendar.setTime(new Date());
-//        System.out.println(calendar.get(Calendar.MONTH));
-
-//        System.out.println("作者:" + myProperties.getAuthor() + " 年龄:" + myProperties.getAge());
 
         System.out.println(RedisMQTest.class.getSimpleName());
     }
