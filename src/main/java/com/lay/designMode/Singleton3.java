2911c8c8393fc23cf0bc9e07afb221810f709cb3
xiaoyueyuea <1406817218@qq.com>
2022-01-12 17:02:28 +0800

    双重校验原因

--- src/main/java/com/lay/designMode/Singleton3.java
+++ src/main/java/com/lay/designMode/Singleton3.java
@@ -20,9 +20,9 @@ public class Singleton3 {
 
     public static Singleton3 getInstance(){
         //未实例化才进入加锁代码
-        if(INSTANCE == null){
+        if(INSTANCE == null){//提高效率
             synchronized (Singleton3.class){
-                if(INSTANCE == null){
+                if(INSTANCE == null){//避免多次实例化
                     //1.为INSTANCE分配内存空间;2.初始化INSTANCE；3.将INSTANCE指向分配的内存地址
                     INSTANCE = new Singleton3();//这行代码实际包含3个指令，在多线程环境下如果没有加volatile可能导致线程不安全
                 }
